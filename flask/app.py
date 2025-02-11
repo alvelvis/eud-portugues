a8fbcd282e1287931d27e7f909b287f7631ddd61
Elvis de Souza <elvis.desouza99@gmail.com>
2025-02-11 09:40:59 -0300

    modificações no app

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, redirect, render_template, request
-import os, subprocess
+from flask import Flask, render_template, request
+import os, subprocess, sys
 import json
 
 app = Flask(__name__)
@@ -7,7 +7,8 @@ app_path = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(app_path, "config.json")
 sentence_path = "sentence.conllu"
 sentence_out = sentence_path.replace('.conllu', '_GREWED.conllu')
-rules_path = 'conjunto_regras_porttinari.grs'
+rules_path = "conjunto_regras_porttinari.grs"
+strategy = "strat_modificadas"
 
 def save_config():
     with open(config_path, "w") as f:
@@ -34,33 +35,8 @@ def home(conllu="", enhancement=""):
     if request.method == "POST":
         # convert new-line to linux style and add empty line in the end
         conllu = request.values.get("inputText").strip().replace("\r\n", "\n") + "\n\n"
-        conllu_lines = conllu.split("\n")
-        for i, line in enumerate(conllu_lines):
-            if "\t" in line:
-                columns = line.split("\t")
-                if len(columns) == 10:
-                    columns[8] = "_"
-                    conllu_lines[i] = "\t".join(columns)
-        conllu = "\n".join(conllu_lines)
-        with open(sentence_path, "w") as f:
-            f.write(conllu)
-        command = f"grew transform -config iwpt -grs \"{rules_path}\" -strat strat_modificadas -i '{sentence_path}' -o '{sentence_out}'"
-        try:
-            enhancement = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ)
-            enhancement.wait()
-            stdout, stderr = enhancement.communicate()
-            if enhancement.returncode != 0:
-                raise subprocess.CalledProcessError(enhancement.returncode, command, stderr)                    
-            with open(sentence_out) as f:
-                enhancement = f.read()
-            os.remove(sentence_out)
-        except subprocess.CalledProcessError as e:
-            enhancement = f"Error executing command: {e.stdout.decode('utf-8')}"
-        except Exception as e:
-            enhancement = f"An unexpected error occurred: {str(e)}"
-        finally:
-            os.remove(sentence_path)
-            increase_access_number(conllu.count("\n\n"))
+        enhancement = annotate(conllu)
+        increase_access_number(conllu.count("\n\n"))
     access_number = config.get("access_number")
     sentences_tested = config.get("sentences_tested")
 
@@ -72,3 +48,42 @@ def home(conllu="", enhancement=""):
         access_number=access_number,
         sentences_tested=sentences_tested
         )
+
+def annotate(conllu):
+    conllu_lines = conllu.split("\n")
+    for i, line in enumerate(conllu_lines):
+        if "\t" in line:
+            columns = line.split("\t")
+            if len(columns) == 10:
+                columns[8] = "_"
+                conllu_lines[i] = "\t".join(columns)
+    conllu = "\n".join(conllu_lines)
+    with open(sentence_path, "w") as f:
+        f.write(conllu)
+    command = f"grew transform -config iwpt -grs \"{rules_path}\" -strat '{strategy}' -i '{sentence_path}' -o '{sentence_out}'"
+    try:
+        enhancement = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ)
+        enhancement.wait()
+        stdout, stderr = enhancement.communicate()
+        if enhancement.returncode != 0:
+            raise subprocess.CalledProcessError(enhancement.returncode, command, stderr)                    
+        with open(sentence_out) as f:
+            enhancement = f.read()
+        os.remove(sentence_out)
+    except subprocess.CalledProcessError as e:
+        enhancement = f"Error executing command: {e.stdout.decode('utf-8')}"
+    except Exception as e:
+        enhancement = f"An unexpected error occurred: {str(e)}"
+    finally:
+        os.remove(sentence_path)
+
+    return enhancement
+
+if __name__ == "__main__":
+    conllu_path = sys.argv[1]
+    rules_path = sys.argv[2]
+    strategy = sys.argv[3]
+    assert all(os.path.exists(x) for x in [conllu_path, rules_path])
+    with open(conllu_path) as f:
+        conllu = f.read()
+    print(annotate(conllu))
